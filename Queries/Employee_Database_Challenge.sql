2bfc9ee8f80b34f693f18a760432ec5cb7baa197
pawankumar1980 <111800568+pawankumar1980@users.noreply.github.com>
2022-10-12 16:46:05 -0400

    additional changes to assignment

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -62,3 +62,49 @@ AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no, ti.from_date DESC;
 
 SELECT * FROM mentorship_eligibilty
+
+------------------------------------------------------------------------------
+-- Deliverable 3
+-----------------------------------------------------------------------------------
+
+-- Roles to be filled when the silver Tsunami begins to impact
+
+--Roles per Staff and Departament: 
+
+SELECT DISTINCT ON (rt.emp_no) 
+	rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title,
+	d.dept_name
+INTO unique_titles_department
+FROM retirement_titles as rt
+INNER JOIN dept_emp as de
+ON (rt.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (d.dept_no = de.dept_no)
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+SELECT * FROM unique_titles_department
+
+-- How many roles will need to be fill per title and department?
+
+SELECT ut.dept_name, ut.title, COUNT(ut.title) 
+INTO rolls_to_fill
+FROM (SELECT title, dept_name from unique_titles_department) as ut
+GROUP BY ut.dept_name, ut.title
+ORDER BY ut.dept_name DESC;
+
+SELECT * FROM rolls_to_fill
+
+
+-- Senior, experienced staff ready to mentor next generation
+
+SELECT ut.dept_name, ut.title, COUNT(ut.title) 
+INTO mentoring_probables
+FROM (SELECT title, dept_name from unique_titles_department) as ut
+WHERE ut.title IN ('Senior Engineer', 'Senior Staff', 'Technique Leader', 'Manager')
+GROUP BY ut.dept_name, ut.title
+ORDER BY ut.dept_name DESC;
+
+SELECT * FROM mentoring_probables
